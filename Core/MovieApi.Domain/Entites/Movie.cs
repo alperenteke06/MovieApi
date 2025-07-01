6ae7d8abf879079e7545648a27fe3733bba85085
alperenteke06 <alperen.teke06@gmail.com>
2025-07-01 13:08:10 +0300

    Domain Katmanı ve Entitylerin tanımlanması.

---
+++ Core/MovieApi.Domain/Entites/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieApi.Domain.Entites
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieCoverImageUrl { get; set; }
+        public decimal MovieRating { get; set; }
+        public string MovieDescription { get; set; }
+        public int MovieDuration { get; set; }
+        public DateTime MovieReleaseDate { get; set; }
+        public string MovieCreatedYear { get; set; }
+        public bool MovieStatus { get; set; }
+    }
+}
---
+++ Core/MovieApi.Domain/Entites/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieApi.Domain.Entites
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewComment { get; set; }
+        public int ReviewUserRating { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public bool ReviewStatus { get; set; }
+    }
+}
