fd27a6c0f2a425c4f4ec692675aa581779af1c91
alperenteke06 <alperen.teke06@gmail.com>
2025-07-01 13:23:06 +0300

    Entitylerin Tamamlanması ve Persistence katmanının hazırlanması.

---
+++ Core/MovieApi.Domain/Entites/Cast.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Domain.Entites
+{
+    public class Cast
+    {
+        public int CastId { get; set; }
+        public string CastTitle { get; set; }
+        public string CastName { get; set; }
+        public string CastSurname { get; set; }
+        public string CastImageUrl { get; set; }
+        public string? CastOverview { get; set; }
+        public string? CastBiography { get; set; }
+    }
+}
---
+++ Core/MovieApi.Domain/Entites/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApi.Domain.Entites
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string TagTitle { get; set; }
+    }
+}
